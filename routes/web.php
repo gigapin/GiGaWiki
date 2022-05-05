6a144d5c1b48e30ca308cd01a0e4fc760462ab80
Giuseppe Galari <gigaprog7@gmail.com>
2022-05-05 21:39:22 +0200

    Test Heroku

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,10 @@ use App\Http\Controllers\DocumentController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', function() {
+    return view('welcome');
+});
+//Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/library', [LibraryController::class, 'index'])->name('library');
 Route::get('/library/{slug}', [LibraryController::class, 'show'])->name('library.show');
 Route::get('/document/{slug}', [DocumentController::class, 'index'])->name('document');
