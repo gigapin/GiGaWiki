bff01f2921c70207aca792f75d30abfa8afa5af1
gigapin <gigaprog7@gmail.com>
2023-01-21 12:41:58 +0100

    Remove VueJS app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,7 +46,6 @@
 
     <!-- ################################### -->
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/main.js') }}"></script>
     <script src="{{ asset('js/jquery.js') }}"></script>
     <script src="{{ asset('summernote/summernote/summernote-lite.min.js') }}"></script>
