ff751ff574fc404e904986c5394c71712db9826b
gigapin <gigaprog7@gmail.com>
2023-02-14 15:13:12 +0100

    Route SErvice updated

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,15 +22,6 @@ class RouteServiceProvider extends ServiceProvider
     public const LOGIN = '/admin/login';
 
     /**
-     * The controller namespace for the application.
-     *
-     * When present, controller route declarations will automatically be prefixed with this namespace.
-     *
-     * @var string|null
-     */
-    // protected $namespace = 'App\\Http\\Controllers';
-
-    /**
      * Define your route model bindings, pattern filters, etc.
      *
      * @return void
