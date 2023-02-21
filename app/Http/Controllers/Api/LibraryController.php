67880af2525d76630d546e97e12bfc5da4ce28ff
gigapin <gigaprog7@gmail.com>
2023-02-21 15:24:14 +0100

    update LibraryController Api

--- app/Http/Controllers/Api/LibraryController.php
+++ app/Http/Controllers/Api/LibraryController.php
@@ -18,34 +18,11 @@ class LibraryController extends Controller
 	public function index()
 	{
 		try {
-			$setting = Setting::where('key', 'allow-public-access')->where('value', 'false')->first();
+			$setting = Setting::where('key', 'allow-public-access')->where('value', 'true')->first();
 			if ($setting === null) {
 				throw new Exception('Forbidden');
 			} else {
 				$subjects = Subject::where('visibility', 1)->get();
-				//dd($subjects[0]);
-				// if (count($subjects) === 1) {
-				// 	//$subject = Subject::where('visibility', 1)->first();
-				// 	$projects = Project::where('subject_id', $subjects[0]->id)->where('visibility', 1)->get();
-				// 	if (count($projects) === 1) {
-				// 		return redirect()->route('document', $projects[0]->slug);
-				// 	}
-					
-				// }
-				
-				// foreach ($subjects as $subject) {
-				// 	if ($subject->image_id !== null) {
-				// 		$image[] = Image::find($subject->image_id)->first();
-				// 	} else {
-				// 		$image[] = null;
-				// 	}
-				// }
-
-				// return response()->json([
-				// 	'subjects' => $subjects,
-				// 	'image' => $image,
-				// 	'default' => [asset('storage/logo-150x150.png')],
-				// ], 200);
 				return response()->json($subjects, 200);
 			}
 		} catch (Exception $exc) {
@@ -62,26 +39,15 @@ class LibraryController extends Controller
 				throw new Exception('Forbidden');
 			} else {
 				$projects = Project::where('subject_id', $id)->where('visibility', 1)->get();
-				
-				//dd($projects);
-				//$image = array();
 				$user = array();
 				foreach ($projects as $project) {
-					// if ($project->image_id !== null) {
-					//$image[] = $project->image->url;
-					// } else {
-					// 	$image[] = null;
-					// }
 					$user[] = $project->user->name;
 				}
 				
 				return response()->json([
 					'projects' => $projects,
 					'user' => $user,
-					//'image' => $image,
-					'default' => Storage::url('pattern_vue.png'),
 				], 200);
-				//return response()->json($projects, 200);
 			}
 		} catch (Exception $exc) {
 			echo $exc->getMessage();
